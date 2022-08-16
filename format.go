f0ca5f8fb8c1dbe4704a0baf78acaf94a24eac03
NICK <tuplecats@gmail.com>
2022-08-16 22:36:55 +0300

    Изменены тесты: один разделен на несколько. Алгоритм учитывает возможные ошибки в файле логов

--- format.go
+++ format.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -27,7 +28,7 @@ type Iformatter interface {
 const (
 	parseBegin = iota
 	parseEvent
-	parseDuration
+	parseUnknown
 	parseKey
 	parseValue
 	parseFinish
@@ -49,12 +50,16 @@ type formatter1C struct {
 	parseValueQuote uint8
 }
 
-func (f *formatter1C) Format(str string) (map[string]string, error) {
-	// cat file.log
-	// Txt='
-	// fdaoijfdsjfiodsjf
-	//'
+type ParseError struct {
+	code int
+	what string
+}
 
+func (e ParseError) Error() string {
+	return fmt.Sprintf("%v:%v", e.code, e.what)
+}
+
+func (f *formatter1C) Format(str string) (map[string]string, error) {
 	if f.currentLine == nil {
 		f.currentLine = make(map[string]string)
 		f.parseValueState = parseValueBegin
@@ -62,36 +67,76 @@ func (f *formatter1C) Format(str string) (map[string]string, error) {
 
 	for {
 		if f.currentState == parseBegin {
-			delim := strings.Index(str, "-")
-			index := strings.Index(str, ",")
+			// Парсим начало строки лога
+			durationBegin := strings.Index(str, "-")
+			if durationBegin == -1 {
+				return nil, ParseError{code: -1, what: "Expected '-'"}
+			}
 
-			f.currentLine["time"] = str[:delim]
+			durationEnd := strings.Index(str, ",")
+			if durationEnd == -1 {
+				return nil, ParseError{code: -1, what: "Expected ','"}
+			}
 
-			minutes := strings.Index(f.currentLine["time"], ":")
-			seconds := strings.Index(f.currentLine["time"], ".")
+			f.currentLine["time"] = str[:durationBegin]
 
-			f.currentLine["minutes"] = f.currentLine["time"][:minutes]
-			f.currentLine["seconds"] = f.currentLine["time"][(minutes + 1):seconds]
-			f.currentLine["duration"] = str[(delim + 1):index]
+			minutesIndex := strings.Index(f.currentLine["time"], ":")
+			secondsIndex := strings.Index(f.currentLine["time"], ".")
+
+			if minutesIndex == -1 || secondsIndex == -1 {
+				return nil, ParseError{code: -1, what: "Minutes and seconds not found"}
+			}
+
+			minutes := f.currentLine["time"][:minutesIndex]
+			minutesDig, err := strconv.Atoi(minutes)
+			if err != nil {
+				return nil, ParseError{code: -1, what: "Invalid minutes value"}
+			} else if minutesDig >= 0 && minutesDig <= 59 {
+				f.currentLine["minutes"] = minutes
+			}
+
+			seconds := f.currentLine["time"][(minutesIndex + 1):secondsIndex]
+			secondsDig, err := strconv.Atoi(seconds)
+			if err != nil {
+				return nil, ParseError{code: -1, what: "Invalid seconds value"}
+			} else if secondsDig >= 0 && secondsDig <= 59 {
+				f.currentLine["seconds"] = seconds
+			}
+
+			duration := str[(durationBegin + 1):durationEnd]
+			if _, err := strconv.Atoi(duration); err != nil {
+				return nil, ParseError{code: -1, what: "Expected digits in duration"}
+			}
+
+			f.currentLine["duration"] = duration
 
 			f.currentState = parseEvent
-			str = str[(index + 1):]
+			str = str[(durationEnd + 1):]
 		} else if f.currentState == parseEvent {
 			index := strings.Index(str, ",")
+			if index == -1 {
+				return nil, ParseError{code: -1, what: "Expected ','"}
+			}
+
 			f.currentLine["event"] = str[:index]
-
-			f.currentState = parseDuration
+			f.currentState = parseUnknown
 			str = str[(index + 1):]
-		} else if f.currentState == parseDuration {
+		} else if f.currentState == parseUnknown {
 			index := strings.Index(str, ",")
-			f.currentLine["duration1"] = str[:index]
+			if index == -1 {
+				return nil, ParseError{code: -1, what: "Expected ','"}
+			}
 
+			//f.currentLine["duration1"] = str[:index]
 			f.currentState = parseKey
 			str = str[(index + 1):]
 		} else if f.currentState == parseKey {
 			index := strings.Index(str, "=")
-			f.currentKey = str[:index]
+			if index == -1 {
+				return nil, ParseError{code: -1, what: "Expected '='"}
+			}
 
+			f.currentKey = str[:index]
 			f.currentState = parseValue
 			f.parseValueState = parseValueBegin
 			str = str[(index + 1):]
@@ -113,13 +158,13 @@ func (f *formatter1C) Format(str string) (map[string]string, error) {
 					}
 				} else if f.parseValueState == parseValueUntil {
 					if len(str) == 0 {
-						return make(map[string]string), errors.New("not finished")
+						return nil, ParseError{code: 0, what: "Not finished"}
 					}
 
 					var index = 0
 					for {
 						if index >= len(str) {
-							return make(map[string]string), errors.New("not finished") //еще не распарсили до конца
+							return nil, ParseError{code: 0, what: "Not finished"} //еще не распарсили до конца
 						}
 
 						if str[index] == '\'' || str[index] == '"' {
@@ -137,6 +182,7 @@ func (f *formatter1C) Format(str string) (map[string]string, error) {
 						index += 1
 					}
 				} else if f.parseValueState == parseValueToNext {
+					// Может быть пустая строка, поэтому после цикла проверяем state
 					for index, symbol := range str {
 						if symbol == '\r' || symbol == '\n' || symbol == ',' {
 							f.currentValue = str[:index]
@@ -146,7 +192,8 @@ func (f *formatter1C) Format(str string) (map[string]string, error) {
 						}
 					}
 
-					if f.parseValueState == parseValueToNext { // По сути это конец строки в принципе
+					// Если пустое значение или достигли конца строки
+					if f.parseValueState == parseValueToNext {
 						f.currentState = parseFinish
 						f.currentLine[f.currentKey] = str
 						break
@@ -167,6 +214,8 @@ func (f *formatter1C) Format(str string) (map[string]string, error) {
 						f.currentState = parseKey
 						str = str[1:]
 						f.currentLine[f.currentKey] = f.currentValue
+					} else {
+						return nil, ParseError{code: -1, what: "Invalid character at end of value"}
 					}
 					break
 				}
@@ -182,44 +231,6 @@ func (f *formatter1C) Format(str string) (map[string]string, error) {
 			return ret, nil
 		}
 	}
-}
-
-//////// Конечный автомат ////////
-
-// Аналог шаблону регулярки \d\d:\d\d\.\d+[-]\d+
-// Но быстрее
-func FSM(str string) bool {
-	defer func() { currentState = stateStart }()
-
-	runes := []rune(str)
-	for i := 0; i < len(runes); i++ {
-		//fmt.Println(str[i:i+1])
-		if isDigital(runes[i]) && (currentState == stateStart || currentState == stateDigital1) {
-			currentState = stateDigital1
-		} else if runes[i] == ':' && currentState == stateDigital1 {
-			currentState = stateDotDot
-		} else if isDigital(runes[i]) && (currentState == stateDotDot || currentState == stateDigital2) {
-			currentState = stateDigital2
-		} else if runes[i] == '.' && currentState == stateDigital2 {
-			currentState = stateDot
-		} else if isDigital(runes[i]) && (currentState == stateDot || currentState == stateDigital3) {
-			currentState = stateDigital3
-		} else if runes[i] == '-' && currentState == stateDigital3 {
-			currentState = stateDash
-		} else if isDigital(runes[i]) && (currentState == stateDash || currentState == stateDigital4) {
-			currentState = stateDigital4
-		} else if runes[i] == ',' && currentState == stateDigital4 {
-			return true
-		} else {
-			return false
-		}
-	}
-
-	return false
-}
-
-func isDigital(letter rune) bool {
-	return letter >= '0' && letter <= '9'
 }
 
 //////////////////////////////////
